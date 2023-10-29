c2fc0df16b580f1022c0621f1246e682ebc960b8
Daniel Walls <walls.dk@gmail.com>
2023-10-28 20:25:17 -0700

    basic framework for main

---
+++ include/types.hpp
@@ -0,0 +1,17 @@
+// Helper type definitions
+// Date: 25-Sep-23
+// Author: Daniel Walls
+// RedID: 825776127
+
+#ifndef ASSIG1_TYPES_H
+#define ASSIG1_TYPES_H
+
+#include <cstdint>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+typedef int32_t s32;
+
+#endif // ASSIG1_TYPES_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,79 @@
 #include <iostream>
+#include <string>
+#include "types.hpp"
 
-int main()
+struct Symbol
 {
-    std::cout << "Hello, world!" << std::endl;
+    std::string name {};
+    std::string address {};
+    std::string flags {};
+};
+
+struct Literal
+{
+    std::string name {};
+    std::string value {};
+    std::string length {};
+    std::string address {};
+};
+
+struct SymbolTableData
+{
+    u32 symbolCount {};
+    Symbol* symbols {};
+
+    u32 literalCount {};
+    Literal* literals {};
+};
+
+struct AssemblyLine
+{
+    std::string address {};
+    std::string label {};
+    std::string instruction {};
+    std::string value1 {};
+    std::string value2 {};
+};
+
+struct ObjectCodeData
+{
+    u32 assemblyLineCount {};
+    AssemblyLine* assemblyLines {};
+};
+
+bool parseSymbolTableFile(const std::string& fileName, SymbolTableData& outData);
+
+bool parseObjectCodeFile(const std::string& fileName, const SymbolTableData& symbolData, ObjectCodeData& outData);
+
+void outputObjectCodeData(const ObjectCodeData& data, const std::string& outputFileName);
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        printf("usage: ./disassem <object code file> <symbol table file>\n");
+        return -1;
+    }
+
+    std::string objectCodeFile {argv[1]};
+    std::string symbolTableFile {argv[2]};
+
+    SymbolTableData symbolTableData {};
+
+    if (!parseSymbolTableFile(symbolTableFile, symbolTableData))
+    {
+        printf("Failed to parse symbol table file!\n");
+        return -2;
+    }
+
+    ObjectCodeData objectCodeData {};
+
+    if (!parseObjectCodeFile(objectCodeFile, symbolTableData, objectCodeData))
+    {
+        printf("Failed to parse object code file!\n");
+        return -3;
+    }
+
+    outputObjectCodeData(objectCodeData, "out.lst");
+    return 0;
 }
